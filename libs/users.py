216c4c946ff3f0e548761969f3bca814e10d91ba
AaryanOberoi <aaryanoberoi22@gmail.com>
2018-05-09 10:49:32 +0530

    Displaying Device and User Count on termainal

--- libs/users.py
+++ libs/users.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
-import pprint
-import pymongo
+# import pprint
+# import pymongo
 client = MongoClient()
 db = client.test_database
 collection = db.test_collection
@@ -14,8 +14,16 @@ collection = db.test_collection
 # def UTC_time_to_epoch(timestamp):
 #     epoch = calendar.timegm(timestamp.utctimetuple())
 #     return epoch
+
 cur = db.test_collection.find()
-print(sorted(list(cur), key)
+a = list(cur)
+b = sorted(a, key=lambda k: k['epoch'], reverse=1)
+print('Device Count :')
+print({b[0]['devicecount']}, {b[0]['date']})
+print('User Count : ')
+print({b[0]['usercount']}, {b[0]['date']})
+# print(sorted(list(cur), 'epoch')
+
 # cur.sort('test_collection.epoch', -1).limit(1)
 # for doc in cur:
-	# print(doc)
+# print(doc)
--- queues.sh
+++ queues.sh
@@ -1,3 +1,4 @@
 docker exec rabbit rabbitmqctl list_queues > devices.txt
 docker exec rabbit rabbitmqctl list_exchanges > users.txt
 python -m libs.upload
+python -m libs.users
